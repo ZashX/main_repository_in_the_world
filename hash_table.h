1c9599247bcfe4cadc8410cdbaf7c3f68f21436c
ZashX <43345180+ZashX@users.noreply.github.com>
2019-03-21 17:09:56 +0300

    Update hash_table.h
    ссылка на сабмит: https://official.contest.yandex.ru/contest/11654/run-report/19715727/

--- hash_table.h
+++ hash_table.h
@@ -6,19 +6,19 @@
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType>>
 class HashMap {
 public:
-  typedef typename std::list<std::pair<const KeyType, ValueType>>::iterator iterator;
-  typedef typename std::list<std::pair<const KeyType, ValueType>>::const_iterator const_iterator;
-
+  typedef typename std::pair<const KeyType, ValueType> pair_kv;
+  typedef typename std::list<pair_kv>::iterator iterator;
+  typedef typename std::list<pair_kv>::const_iterator const_iterator;
 private:
   static const size_t NORM = 16;
   static const size_t MAX_DIFF = 4;
   
   size_t data_size;
-  std::list<std::pair<const KeyType, ValueType>> data;
+  std::list<pair_kv> data;
   std::vector<std::vector<iterator>> hash_table;
   Hash hasher;
 
-  void add_in_data(const std::pair<const KeyType, ValueType>& elem) {
+  void add_in_data(const pair_kv& elem) {
     restructure_map();
     erase_in_data(elem.first);
     data.push_back(elem);
@@ -28,11 +28,11 @@ private:
 
   void erase_in_data(const KeyType& key) {
     restructure_map();
-    size_t ind = hasher(key) % hash_table.size();
-    for (size_t i = 0; i < hash_table[ind].size(); ++i) {
-      if (hash_table[ind][i]->first == key) {
-        data.erase(hash_table[ind][i]);
-        hash_table[ind].erase(hash_table[ind].begin() + i);
+    size_t ind_ht = hasher(key) % hash_table.size();
+    for (size_t ind_cell = 0; ind_cell < hash_table[ind_ht].size(); ++ind_cell) {
+      if (hash_table[ind_ht][ind_cell]->first == key) {
+        data.erase(hash_table[ind_ht][ind_cell]);
+        hash_table[ind_ht].erase(hash_table[ind_ht].begin() + ind_cell);
         data_size--;
         break;
       }
@@ -53,23 +53,16 @@ private:
   }
 
 public:
-  HashMap(const HashMap& _hash) : data_size(_hash.data_size), data(_hash.data), hasher(_hash.hasher) {
+  HashMap& operator =(const HashMap& _hash) {
+    hasher = _hash.hasher;
+    data = std::list<pair_kv>(_hash.data);
+    data_size = _hash.data_size;
     hash_table.clear();
     hash_table.resize(data_size * NORM);
-    for (iterator it = data.begin(); it != data.end(); ++it) {
+    for (auto it = data.begin(); it != data.end(); ++it) {
       hash_table[hasher(it->first) % hash_table.size()].push_back(it);
     }
     restructure_map();
-  }
-
-  HashMap operator =(const HashMap& _hash) {
-    hasher = _hash.hasher;
-    data.clear();
-    data_size = 0;
-    restructure_map();
-    for (auto e : _hash.data) {
-      add_in_data(e);
-    }
     return (*this);
   }
 
@@ -85,7 +78,7 @@ public:
     }
   }
 
-  HashMap(const std::initializer_list<std::pair<KeyType, ValueType>>& init_list, Hash _hasher = Hash()) : data_size(0), hasher(_hasher) {
+  HashMap(const std::initializer_list<pair_kv>& init_list, Hash _hasher = Hash()) : data_size(0), hasher(_hasher) {
     restructure_map();
     for (auto it_cur = init_list.begin(); it_cur != init_list.end(); ++it_cur) {
       add_in_data((*it_cur));
@@ -104,7 +97,7 @@ public:
     return hasher;
   }
 
-  void insert(const std::pair<KeyType, ValueType>& elem) {
+  void insert(const pair_kv& elem) {
     if (find(elem.first) == data.end()) {
       add_in_data(elem);
     }
